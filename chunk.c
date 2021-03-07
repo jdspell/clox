d0c9a7c35a976b3a8aecff552aa30c9fb75e50b0
jdspell <spelljd15@gmail.com>
2021-03-07 11:15:53 -0500

    defined new operations for initalizing a chunk of bytecode and writing to bytecode to the chunk

---
+++ chunk.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+#include "chunk.h"
+
+void initChunk(Chunk* chunk) {
+    chunk->count = 0;
+    chunk->capacity = 0;
+    chunk->code = NULL;
+}
+
+void writeChunk(Chunk* chunk, uint8_t byte) {
+    // if capacity is less than count then:
+    //  Allocate a new array with more capacity.
+    //  Copy the existing elements from the old array to the new one.
+    //  Store the new capacity.
+    //  Delete the old array.
+    //  Update code to point to the new array.
+    //  Store the element in the new array now that there is room.
+    //  Update the count.
+    // else add byte and update count.
+
+
+    if (chunk->capacity < chunk->count + 1) {
+        int oldCapacity = chunk->capacity;
+        chunk->capacity = GROW_CAPACITY(oldCapacity);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code,
+            oldCapacity, chunk->capacity);
+    }
+        chunk->code[chunk->count] = byte;
+        chunk->count ++;
+
+}
